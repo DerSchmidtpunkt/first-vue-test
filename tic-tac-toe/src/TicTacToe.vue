8be559f5e7e6ced99f1e4e5afc3b88430d0e4a3d
DerSchmidtpunkt <schmidtlounge@gmail.com>
2020-01-07 13:46:08 +0000

    tic-tac-toe-init

---
+++ tic-tac-toe/src/TicTacToe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tictactoe-board">
+    <div v-for="(n, i) in 3">
+      <div v-for="(n, j) in 3">
+        <cell :value="board[i][j]"></cell>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() { return {
+      board: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', '']
+        ]
+    } },
+
+  }
+</script>
+<style>
+  .tictactoe-board {
+    display: flex;
+    flex-wrap: wrap;
+    width: 204px;
+    height: 204px;
+  }
+</style>
